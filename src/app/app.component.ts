ed76faac656cb5d76f38b4851ef6fb0dacb1746f
Rajiv <rajivkatariya098@gmail.com>
2024-11-18 22:37:24 +0530

    666666

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-
+import "@fontsource/inter"; 
+import "@fontsource/inter/400.css"; 
+import "@fontsource/inter/400-italic.css"; 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
