459c8fb4fd53f6db041933c7cbc649cd8c0e6fdd
Rajiv <rajivkatariya098@gmail.com>
2024-11-18 21:25:55 +0530

    Ecommblaze

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './Modules/header/header.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { BannerComponent } from './Modules/banner/banner.component';
+import { CategoriesComponent } from './Modules/Categories/Categories.component';
+import { SellItemsComponent } from './Modules/sell-items/sell-items.component';
+import { FooterComponent } from './Modules/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BannerComponent,
+    CategoriesComponent,
+    SellItemsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
